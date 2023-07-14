a76a678b326fd8b4ee9833f7f3a8165054cfba29
emch1690 <33131210+EsthefanieHolguin@users.noreply.github.com>
2023-07-14 02:24:12 -0400

    Sql Server ok

--- conexion_db.py
+++ conexion_db.py
@@ -10,7 +10,7 @@ class ConexionDB:
     def cerrar(self):
         self.connection.commit()
         self.connection.close()
-        print("La conexión ha finalizado.")
+        print("La conexión ha finalizado...")
         
 
 

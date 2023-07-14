62ae238a1a66cbe685f291b18ee27e44f16c0ebe
emch1690 <33131210+EsthefanieHolguin@users.noreply.github.com>
2023-07-14 02:39:21 -0400

    act

--- menu.py
+++ menu.py
@@ -1,9 +1,12 @@
 import tkinter as tk
-from gui.gui_inicio import Frame, barra_menu
+from gui_inicio import Frame, barra_menu
+#import model.trabajador_dao as mt
 #from gui.gui_listado import Frame, barra_menu
 #from gui.gui_nuevo_trabajador import Frame, barra_menu
 def main():
     root = tk.Tk()
+    
+    print("hola")
     
     #Ventana de interfaz de salida
     root.title('EL CORREO DE YURI')
